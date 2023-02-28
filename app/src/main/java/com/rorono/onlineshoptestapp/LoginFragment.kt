2f15f8a9317790f9d546a27dfecb8fa45367470e
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-02-28 11:10:03 +0300

    fix

--- app/src/main/java/com/rorono/onlineshoptestapp/LoginFragment.kt
+++ app/src/main/java/com/rorono/onlineshoptestapp/LoginFragment.kt
@@ -1,7 +1,6 @@
 package com.rorono.onlineshoptestapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +15,7 @@ class LoginFragment : BaseViewBindingFragment<FragmentLoginBinding>(FragmentLogi
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_sig_in_, container, false)
 
     }
 
