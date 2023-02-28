1c7c39a7dedc12b8c69fbed217b3dd07a58eff63
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-02-28 14:27:48 +0300

    secon screen added (Login)

--- app/src/main/java/com/rorono/onlineshoptestapp/LoginFragment.kt
+++ app/src/main/java/com/rorono/onlineshoptestapp/LoginFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rorono.onlineshoptestapp.databinding.FragmentLoginBinding
+import com.rorono.onlineshoptestapp.databinding.FragmentLogInBinding
+
 import com.rorono.onlineshoptestapp.utils.BaseViewBindingFragment
 
 
-class LoginFragment : BaseViewBindingFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+class LoginFragment : BaseViewBindingFragment<FragmentLogInBinding>(FragmentLogInBinding::inflate) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
